037e83c31b450bb48c34ad838f4469dbac5c7587
Ronny Chevalier <ronny@chevalier.io>
2023-01-14 20:43:47 +0100

    runner: add basic support for memory exec on Linux

--- multivers-runner/src/main.rs
+++ multivers-runner/src/main.rs
@@ -3,14 +3,11 @@
 use std::io::Write;
 use std::process::Command;
 
-use anyhow::Context;
+use anyhow::{Context, Result};
 
 use bincode::config;
 
 use flate2::read::DeflateDecoder;
-
-#[cfg(unix)]
-use std::os::unix::fs::PermissionsExt;
 
 mod build;
 
@@ -18,7 +15,7 @@ use build::Build;
 
 const BUILDS: &[u8] = include_bytes!(env!("CARGO_MULTIVERS_BUILDS_PATH"));
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let supported_features: Vec<&str> = std_detect::detect::features()
         .filter_map(|(feature, supported)| supported.then_some(feature))
         .collect();
@@ -41,9 +38,46 @@ fn main() -> anyhow::Result<()> {
             anyhow::anyhow!("Failed to find a build satisfying the current CPU's features")
         })?;
 
+    // On Linux, we first try with `memfd_create` and `execveat` to perform in-memory execution without
+    // relying on temporary files.
+    #[cfg(target_os = "linux")]
+    {
+        use std::ffi::CString;
+        use std::fs::File;
+        use std::os::fd::{FromRawFd, IntoRawFd};
+        use std::os::unix::prelude::OsStringExt;
+
+        use nix::sys::memfd::{memfd_create, MemFdCreateFlag};
+        use nix::unistd::fexecve;
+
+        let memfd_name = CString::new("cargo-multivers")?;
+        let mut file = memfd_create(&memfd_name, MemFdCreateFlag::empty())
+            .map(|fd| unsafe { File::from_raw_fd(fd) })
+            .context("Failed to create an anomymous memory file")?;
+        file.write_all(build.as_bytes())
+            .context("Failed to write the build to an anomymous memory file")?;
+
+        let args = std::env::args_os()
+            .map(|arg| CString::new(arg.into_vec()).map_err(Into::into))
+            .collect::<Result<Vec<CString>>>()?;
+        let env = std::env::vars_os()
+            .map(|(mut key, value)| {
+                key.push("=");
+                key.push(value);
+
+                CString::new(key.into_vec()).map_err(Into::into)
+            })
+            .collect::<Result<Vec<CString>>>()?;
+        let _ = fexecve(file.into_raw_fd(), &args, &env);
+        // If fexecve failed, let's try with a temporary file.
+    }
+
     let mut file = tempfile::NamedTempFile::new().context("Failed to create a temporary file")?;
+    // On Linux, execution with a temporary file will likely fail since it is common for `/tmp` to be mounted with noexec.
     #[cfg(unix)]
     {
+        use std::os::unix::fs::PermissionsExt;
+
         let metadata = file.as_file().metadata()?;
         let mut permissions = metadata.permissions();
 
