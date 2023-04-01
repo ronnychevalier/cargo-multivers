974aeb5164357fe9098695c207344d357046a672
Ronny Chevalier <ronny@chevalier.io>
2023-04-01 18:39:03 +0200

    multivers-runner: move the code finding a version in a method of Build

--- multivers-runner/src/build.rs
+++ multivers-runner/src/build.rs
@@ -2,22 +2,35 @@ use std::io::Write;
 
 use flate2::read::DeflateDecoder;
 
+include!(concat!(env!("OUT_DIR"), "/builds.rs"));
+
 /// Stores a build and the CPU features it requires
 pub struct Build<'a> {
-    pub compressed_build: &'a [u8],
+    compressed_build: &'a [u8],
 
-    pub features: &'a [&'a str],
+    features: &'a [&'a str],
 }
 
 impl<'a> Build<'a> {
-    pub const fn required_cpu_features(&self) -> &[&str] {
-        &self.features
-    }
-
     /// Decompresses the build into a writer
     pub fn decompress_into(&self, mut output: impl Write) -> std::io::Result<()> {
         let mut decoder = DeflateDecoder::new(self.compressed_build);
 
         std::io::copy(&mut decoder, &mut output).map(|_| ())
+    }
+
+    /// Finds a version that matches the CPU features of the host
+    pub fn find() -> Option<Self> {
+        let supported_features: Vec<&str> = std_detect::detect::features()
+            .filter_map(|(feature, supported)| supported.then_some(feature))
+            .collect();
+
+        BUILDS.into_iter().find_map(|build| {
+            build
+                .features
+                .iter()
+                .all(|feature| supported_features.contains(feature))
+                .then_some(build)
+        })
     }
 }
--- multivers-runner/src/main.rs
+++ multivers-runner/src/main.rs
@@ -9,25 +9,10 @@ mod r#impl;
 use build::Build;
 use r#impl::exec;
 
-include!(concat!(env!("OUT_DIR"), "/builds.rs"));
-
 fn main() -> Result<()> {
-    let supported_features: Vec<&str> = std_detect::detect::features()
-        .filter_map(|(feature, supported)| supported.then_some(feature))
-        .collect();
-
-    let build = BUILDS
-        .into_iter()
-        .find_map(|build| {
-            build
-                .required_cpu_features()
-                .iter()
-                .all(|feature| supported_features.contains(feature))
-                .then_some(build)
-        })
-        .ok_or_else(|| {
-            anyhow::anyhow!("Failed to find a build satisfying the current CPU's features")
-        })?;
+    let build = Build::find().ok_or_else(|| {
+        anyhow::anyhow!("Failed to find a build satisfying the host CPU features")
+    })?;
 
     let exe_filename = std::env::args_os()
         .next()
