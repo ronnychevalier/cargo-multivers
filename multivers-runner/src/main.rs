38b0b3224bfbac30b104d9c61b3056824f14da28
Ronny Chevalier <ronny@chevalier.io>
2023-06-06 23:27:16 +0200

    multivers-runner: add missing extern "C" to main

--- multivers-runner/src/main.rs
+++ multivers-runner/src/main.rs
@@ -6,7 +6,7 @@ mod build;
 use build::{Build, Executable};
 
 #[no_mangle]
-pub unsafe fn main(argc: i32, argv: *const *const i8, envp: *const *const i8) {
+pub unsafe extern "C" fn main(argc: i32, argv: *const *const i8, envp: *const *const i8) {
     let result = run(argc, argv, envp);
 
     proc_exit::exit(result);
